04d39f2b3f18b52fcbf9d4944204587ffcf2da9d
valeriya slektor <valeriyaslektor2000@gmail.com>
2025-04-21 14:01:14 +0600

    Update Delivery script: add console messages for package pickup and delivery

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+  bool hasPackage;
+  private void Start() 
+  {
+    Debug.Log(hasPackage);
+  }
+
 void OnCollisionEnter2D(Collision2D other) 
   {
     Debug.Log("Oops!");
@@ -13,13 +19,14 @@ void OnCollisionEnter2D(Collision2D other)
   {
     if (other.tag == "Package")
     {
-
       Debug.Log("Package picked up");
+      hasPackage = true;
     }
 
-    if (other.tag == "Customer")
+    if (other.tag == "Customer" && hasPackage)
     {
       Debug.Log("Package Delivered");
+      hasPackage = false;
     }
   }
 }

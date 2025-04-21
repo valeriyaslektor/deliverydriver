ba51f098883217070f165bbb021969dd4e011eb0
valeriya slektor <valeriyaslektor2000@gmail.com>
2025-04-21 16:01:43 +0600

    Add boost pickup logic: increase speed and apply deceleration on collision

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -6,18 +6,25 @@ public class Driver : MonoBehaviour
 {
 
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
-
-    void Start()
-    {
-       
-    }
-
+    [SerializeField] float moveSpeed =20f;
+    [SerializeField] float slowSpeed = 15f;
+[SerializeField] float boostSpeed = 25f;
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;
+    }
+     void OnTriggerEnter2D(Collider2D other) 
+     {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }

b868f17480616865e36b237f9b462b02f49d7f45
valeriya slektor <valeriyaslektor2000@gmail.com>
2025-04-21 13:43:04 +0600

    Add Delivery script to deliver packages to clients

---
+++ Assets/Delivery.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+void OnCollisionEnter2D(Collision2D other) 
+  {
+    Debug.Log("Oops!");
+  }
+
+  void OnTriggerEnter2D(Collider2D other) 
+  {
+    if (other.tag == "Package")
+    {
+
+      Debug.Log("Package picked up");
+    }
+
+    if (other.tag == "Customer")
+    {
+      Debug.Log("Package Delivered");
+    }
+  }
+}
